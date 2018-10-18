4db3539f28788e77cc1151a5574839cfe6cd82a6
Googa92 <yahorchorny@gmail.com>
2018-10-18 01:31:31 -0700

    full lesson 90

--- app/src/main/java/com/example/yahor/smack/Services/MessageService.kt
+++ app/src/main/java/com/example/yahor/smack/Services/MessageService.kt
@@ -1,10 +1,8 @@
 package com.example.yahor.smack.Services
 
-import android.content.Context
 import android.util.Log
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonArrayRequest
-import com.android.volley.toolbox.Volley
 import com.example.yahor.smack.Controller.App
 import com.example.yahor.smack.Model.Channel
 import com.example.yahor.smack.Utilities.URL_GET_CHANNELS
@@ -14,7 +12,7 @@ object MessageService {
 
     val channels = ArrayList<Channel>()
 
-    fun getChannels(context:Context, complete: (Boolean) -> Unit){
+    fun getChannels(complete: (Boolean) -> Unit){
         val channelsRequest = object: JsonArrayRequest(Method.GET, URL_GET_CHANNELS, null,
             Response.Listener {response ->
                 try {
